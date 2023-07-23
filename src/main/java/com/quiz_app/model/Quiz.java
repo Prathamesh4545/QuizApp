487ecb73ca445d2dad640d6b8d833f1464d04774
Prathamesh date <102506898+Prathamesh4545@users.noreply.github.com>
2023-07-23 15:02:02 +0530

    Update Quiz.java
    Final commit

--- src/main/java/com/quiz_app/model/Quiz.java
+++ src/main/java/com/quiz_app/model/Quiz.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import lombok.Data;
@@ -13,6 +15,7 @@ import lombok.Data;
 public class Quiz {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String title;
 	@ManyToMany
